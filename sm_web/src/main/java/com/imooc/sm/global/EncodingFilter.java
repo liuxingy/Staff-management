6edaea1e9230f811477e43e16b1d0d6dac9b1ece
liuxing <578941470@qq.com>
2020-10-23 17:08:53 +0800

    提交配置文件，过滤器，前端控制器

---
+++ sm_web/src/main/java/com/imooc/sm/global/EncodingFilter.java
@@ -0,0 +1,31 @@
+package com.imooc.sm.global;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author liuxy
+ * @version 1.0
+ * @description:
+ * @date 2020/10/19 17:38
+ */
+public class EncodingFilter implements Filter {
+    private String encoding = "UTF-8";
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        if (filterConfig.getInitParameter("ENCODING") != null)
+        encoding = filterConfig.getInitParameter("ENCODING");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding(encoding);
+        servletResponse.setCharacterEncoding(encoding);
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        encoding = null;
+    }
+}
