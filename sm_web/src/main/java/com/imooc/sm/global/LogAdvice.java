f508f20ef1df98978830aaf9c0cab5376e703a0d
liuxing <578941470@qq.com>
2020-11-10 17:05:36 +0800

    织入失败日志和登录日志

--- sm_web/src/main/java/com/imooc/sm/global/LogAdvice.java
+++ sm_web/src/main/java/com/imooc/sm/global/LogAdvice.java
@@ -4,7 +4,10 @@ import com.imooc.sm.entity.Log;
 import com.imooc.sm.entity.Staff;
 import com.imooc.sm.service.LogService;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,6 +26,7 @@ public class LogAdvice {
 
     @Autowired
     private LogService logService;
+    @After("execution(* com.imooc.sm.controller.*.*(..)) && !execution(* com.imooc.sm.controller.SelfController.*(..)) && !execution(* com.imooc.sm.controller.*.to*.*(..))")
     public void operationLog(JoinPoint joinPoint) {
         Log log = new Log();
         log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
@@ -33,5 +37,49 @@ public class LogAdvice {
         log.setOperator(staff.getAccount());
         log.setResult("成功");
         logService.addOperationLog(log);
+    }
+
+    @AfterThrowing(throwing = "e",pointcut ="execution(* com.imooc.sm.controller.*.*(..)) && !execution(* com.imooc.sm.controller.SelfController.*(..))")
+    public void systemLog(JoinPoint joinPoint, Throwable e) {
+        Log log = new Log();
+        log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
+        log.setOperation(joinPoint.getSignature().getName());
+        HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+        HttpSession session = request.getSession();
+        Staff staff = (Staff) session.getAttribute("USER");
+        log.setOperator(staff.getAccount());
+        log.setResult(e.getClass().getSimpleName());
+        logService.addSystemLog(log);
+    }
+
+    @After("execution(* com.imooc.sm.controller.SelfController.login(..))")
+    public void loginLog(JoinPoint joinPoint) {
+        Log log = new Log();
+        log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
+        log.setOperation(joinPoint.getSignature().getName());
+        HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+        HttpSession session = request.getSession();
+        Staff staff = (Staff) session.getAttribute("USER");
+        if (staff == null) {
+            log.setOperator(request.getParameter("account"));
+            log.setResult("失败");
+        } else {
+            log.setOperator(staff.getAccount());
+            log.setResult("成功");
+        }
+        logService.addLoginLog(log);
+    }
+
+    @Before("execution(* com.imooc.sm.controller.SelfController.logout(..))")
+    public void logoutLog(JoinPoint joinPoint) {
+        Log log = new Log();
+        log.setMoudle(joinPoint.getTarget().getClass().getSimpleName());
+        log.setOperation(joinPoint.getSignature().getName());
+        HttpServletRequest request = (HttpServletRequest) joinPoint.getArgs()[0];
+        HttpSession session = request.getSession();
+        Staff staff = (Staff) session.getAttribute("USER");
+        log.setOperator(staff.getAccount());
+        log.setResult("成功");
+        logService.addLoginLog(log);
     }
 }
